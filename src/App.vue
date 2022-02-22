6532685f5d812df12029424ab83023ebad5bbc41
Kristina Emanuelsson (External - IBM Svenska AB) <kristina.emanuelsson@ingka.com>
2022-02-22 16:57:38 +0100

    More stuff

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-wrapper" :class="{ darkMode: darkMode }">
+    <div class="toolbar">
+      <div class="toggle">
+        <input type="checkbox" v-model="darkMode" /> <label>Dark mode</label>
+      </div>
+    </div>
+    <div class="app-content">
+      <h1>HEMMA</h1>
+      <Product :darkMode="darkMode" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from './components/Product.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Product
+  },
+  data: function () {
+    return {
+      darkMode: false
+    };
+  },
+  watch: {
+    darkMode() {
+      if (this.darkMode) {
+        document.body.classList.add('darkMode');
+      } else {
+        document.body.classList.remove('darkMode');
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=IM+Fell+English:ital@0;1&family=Overpass:wght@400;700;900&display=swap');
+
+.app-wrapper {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+.app-content {
+  width: 70%;
+  margin: auto;
+}
+
+.toolbar {
+  width: 90%;
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+}
+
+h1 {
+  font-size: 8em;
+  font-family: 'Overpass', sans-serif;
+  font-weight: 900;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.darkMode,
+.darkMode h1,
+.darkMode h2 {
+  background: #2c3e50;
+  font-family: 'IM Fell English', sans-serif;
+  color: #ffffff;
+}
+
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="hello">
+    <div class="product-wrapper">
+      <div class="product-image">
+        <img src="../assets/armoire.jpeg">
+      </div>
+      <div class="product-description">
+        <h2>ARMOIRE OF THE BEAST</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum turpis
+          justo, bibendum vehicula nisl vitae, pharetra ultrices arcu. Nullam vel
+          pharetra diam. Nulla mattis turpis justo, vel consequat tortor molestie
+          quis. Etiam convallis ornare justo, nec dapibus leo pellentesque eget.
+          Integer sed vulputate ex. Vestibulum eu justo et massa varius laoreet nec
+          eget dui.
+        </p>
+        <div class="product-price">
+          100 788 :-
+        </div>
+      </div>
+    </div>
+
+    <div class="personal-shopper">
+      <img :src="personalShopper">
+    </div>
+
+
+    <div class="chart-wrapper"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  data: function () {
+    return {
+    };
+  },
+  props: {
+    darkMode: Boolean
+  },
+  computed: {
+    personalShopper() {
+      return this.darkMode ? '../assets/victorian-girl.png' : '../assets/hip-people.jpeg';
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.product-wrapper {
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: row;
+}
+.product-image {
+  width: 40%;
+}
+.product-image img {
+  width: 100%;
+  object-fit: contain;
+}
+.product-description {
+  padding: 0 20px;
+  text-align: left;
+  width: 60%;
+}
+
+h2 {
+  font-family: 'Overpass', sans-serif;
+  font-weight: 900;
+}
+
+p {
+  font-family: 'Overpass', sans-serif;
+  font-weight: 400;
+}
+
+.product-price {
+  float: right;
+  font-family: 'Overpass', sans-serif;
+  font-weight: 900;
+  font-size: 36px;
+}
+
+.personal-shopper {
+  width: 30%;
+  position: fixed;
+  top: 50%;
+  right: 20%;
+}
+
+.personal-shopper img {
+  width: 100%;
+  object-fit: fill;
+}
+
+
+
+</style>
